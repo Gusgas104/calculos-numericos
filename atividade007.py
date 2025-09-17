0f10e5906b2274edf4e4679b06b6985faf2755b0
Gusgas104 <sousabarbosa9998@gmail.com>
2025-09-17 10:13:29 -0300

    Update atividade007.py

--- atividade007.py
+++ atividade007.py
@@ -1,4 +1,10 @@
-n1 = float(input('Digite a primeira nota do aluno: '))
-n2 = float(input('Digite a segunda nota do aluno: '))
-média = (n1 + n2) / 2
-print('A média entre {} e {} é igual a {}'.format(n1, n2, média))
+n1 = int(input('Digite um  número: '))
+n2 = int(input('Digite um número: '))
+a = n1 + n2
+s = n1 - n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print(' A soma é {}, a subtração é {}, o produto é {}, e a divisão é {:.3f} '.format(a, s, m, d))
+print('A divisão interia é {} e a pontência é {}'.format(di, e))
